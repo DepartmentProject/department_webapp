daff786e8d11c06a85ce7df010b8766719af5c4f
susmetaa01 <susmetaa01@gmail.com>
2021-07-14 20:56:46 +0530

    cse dept changes

--- department_project/initial/urls.py
+++ department_project/initial/urls.py
@@ -18,5 +18,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index, name = "index")
+    path('',views.index, name = "index"),
+    path('index',views.index, name = "index"),
+    path('cse',views.cse, name = "computerscience"),
 ]
